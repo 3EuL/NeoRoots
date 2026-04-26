cb02004f7b6f99f6ac21b6c2763bb72dc41d042e
3EuL <kihindrant@gmail.com>
2026-04-26 17:37:31 -0500

    Delete NeoRoots-Design directory

--- NeoRoots-Design/FrontEnd/Nabvar.php
+++
@@ -1,8 +0,0 @@
-<header class="navbar">
-    <div class="logo">🌱 NeoRoots</div>
-    <nav>
-        <a href="#">HOME</a>
-        <a href="#">REPORTS</a>
-        <a href="#">SETTINGS</a>
-    </nav>
-</header>
