fd4579b1da6c0539a632eeacad6b162e6a1b4c15
3EuL <kihindrant@gmail.com>
2026-05-18 16:47:54 -0500

    Add files via upload

--- NeoRoots/JS/scan.js
+++ NeoRoots/JS/scan.js
@@ -1,18 +1,14 @@
 const contenedores = document.querySelectorAll('.contenedor');
 const noti = document.getElementById('notificacion');
 
+const CAM_URL = "http://192.168.4.1/capture";
+
 contenedores.forEach(c => {
-  c.addEventListener('click', () => {
+  c.addEventListener('click', async () => {
 
-    // puntos
     const puntos = c.dataset.points;
-
-    // hora exacta
     const ahora = new Date();
     const hora = ahora.toLocaleTimeString();
-
-    // mensaje
-    const mensaje = `+${puntos} puntos | ${hora}`;
 
     // animación click
     c.style.transform = "scale(0.95)";
@@ -20,13 +16,31 @@ contenedores.forEach(c => {
       c.style.transform = "scale(1)";
     }, 150);
 
-    // mostrar notificación
-    noti.textContent = mensaje;
+    try {
+      // tomar foto desde ESP32
+      const imgURL = CAM_URL + "?t=" + new Date().getTime();
+
+      noti.innerHTML = `
+        +${puntos} puntos | ${hora}<br>
+        <img src="${imgURL}" width="170" style="margin-top:10px; border-radius:10px;">
+      `;
+
+    } catch (error) {
+      noti.innerHTML = `+${puntos} puntos | ${hora}<br>Error al tomar foto`;
+      console.error(error);
+    }
+
     noti.classList.add('show');
 
     setTimeout(() => {
       noti.classList.remove('show');
-    }, 3000);
+    }, 5000);
 
   });
 });
+
+const camara = document.getElementById("camara");
+
+setInterval(() => {
+  camara.src = "http://192.168.4.1/capture?t=" + new Date().getTime();
+}, 100);
