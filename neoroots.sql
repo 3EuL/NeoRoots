b9c7417da9e042c303540a6a8f8024b8922a5d09
3EuL <kihindrant@gmail.com>
2026-05-31 22:21:15 -0500

    Add files via upload

--- neoroots.sql
+++ neoroots.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 14-04-2026 a las 23:39:52
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: Jun 01, 2026 at 05:20 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `neoroots`
+-- Database: `neoroots`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `active_session`
+-- Table structure for table `active_session`
 --
 
 CREATE TABLE `active_session` (
@@ -38,7 +38,21 @@ CREATE TABLE `active_session` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `containers`
+-- Table structure for table `cameras`
+--
+
+CREATE TABLE `cameras` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `ip` varchar(50) NOT NULL,
+  `status` enum('online','offline') DEFAULT 'online',
+  `last_seen` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `containers`
 --
 
 CREATE TABLE `containers` (
@@ -48,10 +62,20 @@ CREATE TABLE `containers` (
   `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Dumping data for table `containers`
+--
+
+INSERT INTO `containers` (`container_id`, `colour`, `waste_type`, `description`) VALUES
+(16, '#ab3f3f', 'Cartón', 'En este contenedor de ___ color se va a guardar el cartón'),
+(18, '#24ff50', 'Plástico', 'En este contenedor de color ___ se van a guardar los residuos plásticos.'),
+(19, '#1492e1', 'Vidrio', 'En este contenedor de color ___ va el vídrio'),
+(22, '#ff0000', 'Metal', 'En este contenedor de color ___ van los residuos metálicos');
+
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `points`
+-- Table structure for table `points`
 --
 
 CREATE TABLE `points` (
@@ -64,7 +88,7 @@ CREATE TABLE `points` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `recycling_log`
+-- Table structure for table `recycling_log`
 --
 
 CREATE TABLE `recycling_log` (
@@ -79,7 +103,7 @@ CREATE TABLE `recycling_log` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `scan_log`
+-- Table structure for table `scan_log`
 --
 
 CREATE TABLE `scan_log` (
@@ -92,7 +116,7 @@ CREATE TABLE `scan_log` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -105,16 +129,19 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `user`, `email`, `pass`, `rol`, `pfp`) VALUES
-(1, 'est1', 'est@gmail.com', '$2y$10$Nwrp1l9O5sEynsj/3byCJOebrjAf20olQqthm17O/QVUV7N.h3v.S', 'usuario', NULL);
+(16, 'DillanVallejos', 'dil4n@gmail.com', '$2y$10$KtJj2aMSYMOBPocVZxmXsOGIDxbjeqVklUqNJmn/SRZbkuF04xxAS', 'usuario', NULL),
+(17, 'esteban.urbano.6', '123444@gmail.com', '$2y$10$abR3/crRmisvPtQQcy0r9eCcX6aol92cP8zt0635kSBKgDcWh8N1e', 'usuario', NULL),
+(18, 'EuL', 'E2222@gmail.com', '$2y$10$ZBAQzwTzN8WeCmcL2GrSkOauAo5lkUehhbDS3nowIsKabuxCyB3p.', 'usuario', '69ee86cb0d9fe_ChatGPT Image Apr 20, 2026, 09_08_56 PM.png'),
+(19, 'admin', '232@gmail.com', '$2y$10$v4IqmfgnQf2QjNvE5X0xWezykI09CtNXRcEXwxkvMFj.w9qI4h1MC', 'admin', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `waste`
+-- Table structure for table `waste`
 --
 
 CREATE TABLE `waste` (
@@ -125,107 +152,149 @@ CREATE TABLE `waste` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `active_session`
+-- Indexes for table `active_session`
 --
 ALTER TABLE `active_session`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_session_user` (`user_id`),
+  ADD KEY `fk_session_container` (`container_id`);
 
 --
--- Indices de la tabla `containers`
+-- Indexes for table `cameras`
+--
+ALTER TABLE `cameras`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `containers`
 --
 ALTER TABLE `containers`
   ADD PRIMARY KEY (`container_id`);
 
 --
--- Indices de la tabla `points`
+-- Indexes for table `points`
 --
 ALTER TABLE `points`
   ADD KEY `fk_points_user` (`user_id`);
 
 --
--- Indices de la tabla `recycling_log`
+-- Indexes for table `recycling_log`
 --
 ALTER TABLE `recycling_log`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_log_user` (`user_id`),
+  ADD KEY `fk_log_container` (`container_id`),
+  ADD KEY `fk_log_waste` (`waste_id`);
 
 --
--- Indices de la tabla `scan_log`
+-- Indexes for table `scan_log`
 --
 ALTER TABLE `scan_log`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_scan_user` (`user_id`),
+  ADD KEY `fk_scan_container` (`container_id`);
 
 --
--- Indices de la tabla `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- Indices de la tabla `waste`
+-- Indexes for table `waste`
 --
 ALTER TABLE `waste`
   ADD PRIMARY KEY (`waste_id`),
   ADD KEY `fk_waste_container` (`container_id`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `active_session`
+-- AUTO_INCREMENT for table `active_session`
 --
 ALTER TABLE `active_session`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `containers`
+-- AUTO_INCREMENT for table `cameras`
 --
-ALTER TABLE `containers`
-  MODIFY `container_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `cameras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `recycling_log`
+-- AUTO_INCREMENT for table `containers`
+--
+ALTER TABLE `containers`
+  MODIFY `container_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `recycling_log`
 --
 ALTER TABLE `recycling_log`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `scan_log`
+-- AUTO_INCREMENT for table `scan_log`
 --
 ALTER TABLE `scan_log`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT de la tabla `waste`
+-- AUTO_INCREMENT for table `waste`
 --
 ALTER TABLE `waste`
   MODIFY `waste_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `points`
+-- Constraints for table `active_session`
+--
+ALTER TABLE `active_session`
+  ADD CONSTRAINT `fk_session_container` FOREIGN KEY (`container_id`) REFERENCES `containers` (`container_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_session_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `points`
 --
 ALTER TABLE `points`
-  ADD CONSTRAINT `fk_points_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_points_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
 
 --
--- Filtros para la tabla `waste`
+-- Constraints for table `recycling_log`
+--
+ALTER TABLE `recycling_log`
+  ADD CONSTRAINT `fk_log_container` FOREIGN KEY (`container_id`) REFERENCES `containers` (`container_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_log_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_log_waste` FOREIGN KEY (`waste_id`) REFERENCES `waste` (`waste_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `scan_log`
+--
+ALTER TABLE `scan_log`
+  ADD CONSTRAINT `fk_scan_container` FOREIGN KEY (`container_id`) REFERENCES `containers` (`container_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_scan_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `waste`
 --
 ALTER TABLE `waste`
-  ADD CONSTRAINT `fk_waste_container` FOREIGN KEY (`container_id`) REFERENCES `containers` (`container_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_waste_container` FOREIGN KEY (`container_id`) REFERENCES `containers` (`container_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
